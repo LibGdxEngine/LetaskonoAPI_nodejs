0bd7bee83b59c334186b87c3ec406102629141ac
A.Ramadan.ext <libgdxengine@gmail.com>
2023-01-29 21:35:55 +0200

    add get list of users 4

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,5 @@
 const userService = require("../services/UserService");
- 
+
 exports.getAllUsers = async (req, res) => {
   try {
     const users = await userService.getAllUsers();
@@ -8,26 +8,26 @@ exports.getAllUsers = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
- 
+
 exports.createUser = async (req, res) => {
   try {
-    const phone = req.body['phone'];
+    const phone = req.body["phone"];
     const otherUser = await userService.getUserByPhone(phone);
-    if(otherUser == null){
+    if (otherUser == null) {
       const user = await userService.createUser(req.body);
       res.json({ data: user, status: "success" });
-    }else{
+    } else {
       res.status(500).json({ error: "هذا الرقم مسجل لدينا بالفعل" });
     }
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
- 
+
 exports.getUserById = async (req, res) => {
   try {
     let user = await userService.getUserByPhone(req.params.id);
-    if(user == null){
+    if (user == null) {
       user = await userService.getUserById(req.params.id);
     }
     res.json({ data: user, status: "success" });
@@ -35,7 +35,7 @@ exports.getUserById = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
- 
+
 exports.updateUser = async (req, res) => {
   try {
     const user = await userService.updateUser(req.params.id, req.body);
@@ -44,7 +44,7 @@ exports.updateUser = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
- 
+
 exports.deleteUser = async (req, res) => {
   try {
     const user = await userService.deleteUser(req.params.id);
@@ -54,36 +54,40 @@ exports.deleteUser = async (req, res) => {
   }
 };
 
-exports.getListOfUsers = async (req, res) =>{
-  try{
+exports.getListOfUsers = async (req, res) => {
+  try {
     const usersList = [];
-    const userPhonesList = req.body['userPhonesList'];
+    const userPhonesList = req.body["userPhonesList"];
 
     for (let i = 0; i < userPhonesList.length; i++) {
-      let userId = userPhonesList[i]
-      const user = await userService.getUserByPhone(userId);
-      if(user != null){
+      let userId = userPhonesList[i];
+
+      let user = await userService.getUserByPhone(req.params.id);
+      if (user == null) {
+        user = await userService.getUserById(req.params.id);
+      }
+      if (user != null) {
         usersList.push(user);
       }
     }
 
-    res.json({data: usersList, status: "success"}) ;
+    res.json({ data: usersList, status: "success" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
-exports.timeSpan = async (req, res) =>{
-  try{
-    const phone = req.body['phone'];
+exports.timeSpan = async (req, res) => {
+  try {
+    const phone = req.body["phone"];
     const user = await userService.getUserByPhone(phone);
-    
-    var d1 = user.acceptance.updatedAt;  
 
-    var diff = Date.now() - new Date(d1) ;
-        
-    var daydiff = diff / (1000 * 60 * 60 * 24);   
-    res.json({timeSpan: daydiff, status: "success"}) ;
+    var d1 = user.acceptance.updatedAt;
+
+    var diff = Date.now() - new Date(d1);
+
+    var daydiff = diff / (1000 * 60 * 60 * 24);
+    res.json({ timeSpan: daydiff, status: "success" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
